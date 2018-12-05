9a74f8dfec55a30e190b577a26d86d71362a46a9
Carlo Corti <araknoid@gmail.com>
2018-12-05 23:08:20 +0100

    [Day02] Answer part 1

--- src/test/kotlin/Day02Test.kt
+++ src/test/kotlin/Day02Test.kt
@@ -3,10 +3,19 @@ import org.junit.jupiter.api.Test
 
 class Day02Test {
 
+    val day02 = Resources.linesFrom("day02.txt")
+
     @Test
     fun `example of checksum`() {
         val checksum: Int = Day02.checksum(listOf("abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"))
 
         assertThat(checksum).isEqualTo(12)
+    }
+
+    @Test
+    fun `answer part 1`() {
+        val checksum: Int = Day02.checksum(day02)
+
+        assertThat(checksum).isEqualTo(7350)
     }
 }
